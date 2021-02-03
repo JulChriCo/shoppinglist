aacb806381d29054b6b07932d73639dc611fa6f2
codewithjulie <codewithjulie@gmail.com>
2021-02-02 22:16:07 -0800

    fix

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 Hi Julie :)  You are a special person.  Just thought you should know :)
 
-<<<<<<< HEAD
 I also want to let you know you are a special person!
-=======
 I changed something
->>>>>>> a61481183be990efaecf04e8a52f5bb75ffb6735
